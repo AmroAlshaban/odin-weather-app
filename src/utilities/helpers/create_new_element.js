3ab697b90a037b3a70ab743a3131efad8ee3c978
AmroAlshaban <140441695+AmroAlshaban@users.noreply.github.com>
2025-01-14 21:14:03 +0300

    Created a helper function (createNewElement) that makes generating html elements faster and more dynamic.

---
+++ src/utilities/helpers/create_new_element.js
@@ -0,0 +1,16 @@
+export function createNewElement({nameTag, className='', idName='', textContent='', attributes={}} = {}) {
+    const newElement = document.createElement(nameTag);
+    if (className !== '') {
+        newElement.classList.add(className);
+    };
+    if (idName !== '') {
+        newElement.id = idName;
+    };
+    newElement.textContent = textContent;
+
+    for (let attribute in attributes) {
+        newElement.setAttribute(attribute, attributes[attribute]);
+    };
+
+    return newElement;
+};
