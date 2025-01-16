5f4ea824b12a798e5e08049313a8b551d7724d2d
AmroAlshaban <140441695+AmroAlshaban@users.noreply.github.com>
2025-01-16 15:44:06 +0300

    Added media queries for <header>.

---
+++ src/ui/search/createSearchBar.js
@@ -0,0 +1,16 @@
+import { createNewElement } from "../../utilities/helpers/create_new_element";
+
+
+function createSection1() {
+    
+}
+
+
+function createSearchBar() {
+
+};
+
+
+function createSearchBarImage() {
+
+};
